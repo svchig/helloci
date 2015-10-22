75f3a7a82457a022a3b1c3e5c600b4422ad0d60a
Siarhei Chyhir <Siarhei.Chyhir@gmail.com>
2015-10-22 16:23:03 +0300

    testShouldBeFailed was updated

--- Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
+++ Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
@@ -17,7 +17,7 @@ public class AppTest {
 	}
 
 	@Test
-	public void testShouldBeFailed() {assertFalse(false);
+	public void testShouldBeFailed() {assertTrue(false);
 	}
 
 	@Ignore("Not implemented yet")
